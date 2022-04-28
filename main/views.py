37181ebd020910d41d05b04c57dff339e5b02a33
Venkateswarlu Tangella <t.venkat.cse@gmail.com>
2022-04-28 15:08:58 +0530

    new update

--- main/views.py
+++ main/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 import requests
 import sys
 import os
+import time
 from subprocess import run,PIPE
 from django.core.files.storage import FileSystemStorage
 from django.http import HttpResponse
 from django.contrib.staticfiles import finders
 from main.settings import ALGO_FOLDER
+
 
 
 def button(request):
@@ -22,14 +24,17 @@ def external(request):
     filename=fs.save(image.name,image)
     fileurl=fs.open(filename)
     templateurl=fs.url(filename)
-    print("Algorithm:-")
-    if (technique=="1"):print("Principle Component Analysis")
-    else:print("Incremental Principle Component Analysis")
+    print(f'Number of Components:- {no_of_samples}')
+    if (technique=="1"):print("Technique:- Principle Component Analysis")
+    else:print("Technique:- Incremental Principle Component Analysis")
     print("File_Name:-",filename)
-    print("Full_Path:-", fileurl)
+   # print("Full_Path:-", fileurl)
     print("Templete_Path:-",templateurl)
+    start = time.time()
     image= run([sys.executable,ALGO_FOLDER+'algo.py',str(fileurl),str(filename),str(no_of_samples),str(technique)],shell=False,stdout=PIPE)
     print(image.stdout)
+    end = time.time()
+    print(end-start)
     
     f1 = open(ALGO_FOLDER+'size1.txt', 'r')
     f2 = open(ALGO_FOLDER+'size2.txt', 'r')
